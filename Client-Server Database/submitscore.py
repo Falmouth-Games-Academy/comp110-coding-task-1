a659035cb9fe19c6bface6a235f94a4db018a4eb
NecroReindeer <necroreindeer@gmail.com>
2016-01-14 21:14:53 +0000

    Added code for submitting a score

---
+++ Client-Server Database/submitscore.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# Turn on debug mode
+import cgitb
+import cgi
+import json
+import pymysql
+
+import id
+
+cgitb.enable()
+
+# Print necessary headers
+print("Content-Type: text/html; charset=utf-8\n\n")
+
+form = cgi.FieldStorage()
+
+player_name = str(form.getvalue("player")).upper()
+level_number = form.getvalue("level")
+score = form.getvalue("score")
+
+# Connect to the database
+conn = pymysql.connect(
+        db='highscores',
+        user='root',
+        passwd='falser110',
+        host='localhost')
+c = conn.cursor()
+
+player_ids = id.get_player_id(player_name)
+player_id = player_ids[0]
+
+c.execute("INSERT INTO scores_test VALUES(" + str(player_id) + ", " + level_number + ", " + score + ")")
+conn.commit()
+
+c.execute("SELECT * FROM scores_test")
+highscores = [(row[0], row[1], row[2]) for row in c.fetchall()]
+print(json.dumps(highscores))
