58df36ea05979b67cc9525a7afa07ab12bc5b85c
Sam Wills <samwillsacc@gmail.com>
2016-01-15 05:21:52 +0000

    Create Top_score.py

---
+++ Top_score.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+#Turn on debug mode.
+import cgitb
+cgitb.enable()
+
+#Print necessary headers.
+print("Content-Type: text/html; charset=utf-8\n\n")
+
+#Connect to the database.
+import cgi
+form = cgi.FieldStorage()
+  
+#Connect to the database.
+import pymysql
+conn = pymysql.connect(
+  db = 'example',
+  user = 'root'
+  passwd = 'password' #changed this incase it matter
+  host = 'localhost')
+c = conn.cursor()
+
+
+#Get some example data
+sql = "SELECT 'first_name', 'last_name', 'score' 
+FROM scores 
+ORDER BY scores DESC
+LIMIT 1"
+
+
+c.execute(sql, ("bsccg03.ga.fal.io/",))
+results = c.fetchall()
+print(result)
+
+connection.close()
