8aaad1ff1cc81fa91da7ff2dcfcbe3ac5b79bd48
NecroReindeer <necroreindeer@gmail.com>
2016-01-18 20:30:26 +0000

    Restructured getscores and gethighest
    gethighest.py is now split into functions.
    getscores.py now retrives the level number in the main bit.
    Both print None if a field was not included in the form.

--- Client-Server Database/gethighest.py
+++ Client-Server Database/gethighest.py
@@ -11,31 +11,32 @@ import json
 
 import pymysql
 
-# Print necessary headers
-print("Content-Type: text/html; charset=utf-8\n\n")
+import processing
 
-form = cgi.FieldStorage()
 
-if "player" not in form or "level" not in form:
-    print(json.dumps("Player or level not provided"))
-else:
-    player_name = str(form.getvalue("player")).upper()
-    level_number = form.getvalue("level")
+def get_best_score(player_name, level_number):
 
-# Connect to the database
-conn = pymysql.connect(
-        db='highscores',
-        user='root',
-        passwd='falser110',
-        host='localhost')
-cursor = conn.cursor()
+    connection, cursor = processing.connect_to_database()
 
-cursor.execute("SELECT players_test.name, levels_test.level_name, scores_test.score FROM scores_test "
-               "INNER JOIN players_test ON scores_test.player_id = players_test.player_id "
-               "INNER JOIN levels_test ON scores_test.level_id = levels_test.level_id "
-               "WHERE players_test.name='" + player_name + "' AND levels_test.level_id=" + level_number +
-               " ORDER BY score DESC LIMIT 1")
+    cursor.execute("SELECT players_test.name, levels_test.level_name, scores_test.score FROM scores_test "
+                   "INNER JOIN players_test ON scores_test.player_id = players_test.player_id "
+                   "INNER JOIN levels_test ON scores_test.level_id = levels_test.level_id "
+                   "WHERE players_test.name='" + player_name + "' AND levels_test.level_id=" + level_number +
+                   " ORDER BY score DESC LIMIT 1")
 
-highscores = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
-print(json.dumps(highscores))
+    best_score = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
+    return best_score
 
+if __name__ == "__main__":
+    # Print necessary headers
+    print("Content-Type: text/html; charset=utf-8\n\n")
+
+    form = cgi.FieldStorage()
+    player_name = processing.get_player_name(form)
+    level_number = processing.get_level_number(form)
+
+    if player_name != None and level_number != None:
+        best_score = get_best_score(player_name, level_number)
+        print(json.dumps(best_score))
+    else:
+        print(json.dumps(None))
--- Client-Server Database/getscores.py
+++ Client-Server Database/getscores.py
@@ -11,15 +11,10 @@ import pymysql
 
 import processing
 
-def get_level():
-    form = cgi.FieldStorage()
-    level_number = processing.get_level_number(form)
-    return level_number
-
 def get_top_ten(level_number):
     connection, cursor = processing.connect_to_database()
 
-    # Get the top 10 high scores from the database
+    # Get the top 10 high scores from the database, list the highest first
     cursor.execute("SELECT players_test.name, levels_test.level_name, score FROM scores_test "
                    "INNER JOIN players_test ON scores_test.player_id = players_test.player_id "
                    "INNER JOIN levels_test ON scores_test.level_id = levels_test.level_id "
@@ -31,11 +26,15 @@ def get_top_ten(level_number):
 
 if __name__ == "__main__":
     cgitb.enable()
-    
     # Print necessary headers
     print("Content-Type: text/html; charset=utf-8\n\n")
 
-    level = get_level()
-    highscores = get_top_ten(level)
-    print(json.dumps(highscores))
+    form = cgi.FieldStorage()
+    level_number = processing.get_level_number(form)
+
+    if level_number != None:
+        highscores = get_top_ten(level_number)
+        print(json.dumps(highscores))
+    else:
+        print(json.dumps(None))
 
