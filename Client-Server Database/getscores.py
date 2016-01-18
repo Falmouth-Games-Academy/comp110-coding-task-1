b83f4e9e4841ef44599bea360f8c6d9c4d8909e2
NecroReindeer <necroreindeer@gmail.com>
2016-01-18 17:40:32 +0000

    Made highscores for a given level be retrieved
    Made getscores.py retrieve high scores for the given level instead of
    all levels.
    Put cgi form processing into processing.py.
    Split getscores.py into functions.

--- Client-Server Database/getscores.py
+++ Client-Server Database/getscores.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python
 
+"""Respond with a list of the the top 10 high scores.
+"""
+
+import cgi
+import cgitb
 import json
 
 import pymysql
 
-# Print necessary headers
-print("Content-Type: text/html; charset=utf-8\n\n")
+import processing
 
-# Connect to the database
-conn = pymysql.connect(
-        db='highscores',
-        user='root',
-        passwd='falser110',
-        host='localhost')
-cursor = conn.cursor()
+def get_level():
+    form = cgi.FieldStorage()
+    level_number = processing.get_level_number(form)
+    return level_number
 
-# Get the top 10 high scores from the database
-cursor.execute("SELECT players_test.name, levels_test.level_name, score FROM scores_test "
-          "INNER JOIN players_test ON scores_test.player_id = players_test.player_id "
-          "INNER JOIN levels_test ON scores_test.level_id = levels_test.level_id "
-          "ORDER BY score DESC LIMIT 10")
+def get_top_ten(level_number):
+    connection, cursor = processing.connect_to_database()
 
-highscores = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
-print(json.dumps(highscores))
+    # Get the top 10 high scores from the database
+    cursor.execute("SELECT players_test.name, levels_test.level_name, score FROM scores_test "
+                   "INNER JOIN players_test ON scores_test.player_id = players_test.player_id "
+                   "INNER JOIN levels_test ON scores_test.level_id = levels_test.level_id "
+                   "WHERE scores_test.level_id='" + level_number + "' "
+                   "ORDER BY score DESC LIMIT 10")
+
+    highscores = [(row[0], row[1], row[2]) for row in cursor.fetchall()]
+    return highscores
+
+if __name__ == "__main__":
+    cgitb.enable()
+    
+    # Print necessary headers
+    print("Content-Type: text/html; charset=utf-8\n\n")
+
+    level = get_level()
+    highscores = get_top_ten(level)
+    print(json.dumps(highscores))
 
--- Client-Server Database/processing.py
+++ Client-Server Database/processing.py
@@ -3,7 +3,31 @@
 This module contains functions for processing data.
 """
 
+import cgi
+
 import pymysql
+
+def get_player_name(form):
+    if "player" not in form:
+        return None
+    else:
+        player_name = str(form.getvalue("player")).upper()
+        player_name = chop_name(player_name, 3)
+        return player_name
+
+def get_level_number(form):
+    if "level" not in form:
+        return None
+    else:
+        level_number = form.getvalue("level")
+        return level_number
+
+def get_score(form):
+    if "score" not in form:
+        return None
+    else:
+        score = form.getvalue("score")
+        return score
 
 def chop_name(name, characters):
     chopped_name = name[:characters]
@@ -11,15 +35,20 @@ def chop_name(name, characters):
 
 def get_player_id(player_name):
     # Connect to the database
-    conn = pymysql.connect(
-            db='highscores',
-            user='root',
-            passwd='falser110',
-            host='localhost')
-    cursor = conn.cursor()
-
+    connection, cursor = connect_to_database()
 
     cursor.execute("SELECT players_test.player_id FROM players_test "
                    "WHERE players_test.name='" + player_name + "'")
     player_id = [row[0] for row in cursor.fetchall()]
     return player_id
+
+def connect_to_database():
+    # Connect to the database
+    connection = pymysql.connect(
+            db='highscores',
+            user='root',
+            passwd='falser110',
+            host='localhost')
+    cursor = connection.cursor()
+    return connection, cursor
+
