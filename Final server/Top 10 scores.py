ec93abaa33e6622f3ef97e2d44c40864c4baa207
Sam Wills <samwillsacc@gmail.com>
2016-01-24 14:28:18 +0000

    Update Top 10 scores.py

--- Final server/Top 10 scores.py
+++ Final server/Top 10 scores.py
@@ -1,3 +1,18 @@
+#!/usr/bin/python
+
+# Turn on debug mode.
+import cgitb
+import pymysql
+import json
+
+cgitb.enable()
+
+# Print necessary headers.
+print("Content-Type: text/html; charset=utf-8\n\n")
+
+import cgi
+form = cgi.FieldStorage()
+
 def print_highscores():
     c.execute("SELECT players.first_name, players.last_name, scores.score FROM scores INNER JOIN players ON player$
     print ("<b>Top 10 scores</b><br />")
