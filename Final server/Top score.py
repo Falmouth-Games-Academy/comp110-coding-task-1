8c126284c3533b83c6ae1317c2b3823f3dea80c8
Sam Wills <samwillsacc@gmail.com>
2016-01-24 14:34:04 +0000

    Update Top score.py

--- Final server/Top score.py
+++ Final server/Top score.py
@@ -1,5 +1,5 @@
 def print_highscore():
-    c.execute("SELECT players.first_name, players.last_name, scores.score FROM scores INNER JOIN players ON player$
+    c.execute("SELECT players.first_name, players.last_name, scores.score FROM scores INNER JOIN players ON player_id = players.id ORDER BY score DESC LIMIT 1")
     print ("<b>Top score</b><br />")
     scores = ([(r[0], r[1], r[2]) for r in c.fetchall()])
     print (json.dumps(scores))
