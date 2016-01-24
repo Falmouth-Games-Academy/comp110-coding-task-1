be85ef3d21d92bd5c915c127b57e06b660705fee
Sam Wills <samwillsacc@gmail.com>
2016-01-24 14:46:17 +0000

    Update Top 10 scores.py

--- Final server/Top 10 scores.py
+++ Final server/Top 10 scores.py
@@ -13,6 +13,14 @@ print("Content-Type: text/html; charset=utf-8\n\n")
 import cgi
 form = cgi.FieldStorage()
 
+# Connect to the database.
+conn = pymysql.connect(
+    db='highscores',
+    user='root',
+    passwd='samwillsfal',
+    host='localhost')
+c = conn.cursor()
+
 def print_highscores():
     c.execute("SELECT players.first_name, players.last_name, scores.score FROM scores INNER JOIN players ON player_id = players.id ORDER BY score DESC LIMIT 10")
     print ("<b>Top 10 scores</b><br />")
