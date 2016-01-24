785541d1ecb65549058af1bbfe518bbb24fe04a1
Sam Wills <samwillsacc@gmail.com>
2016-01-24 14:35:15 +0000

    Update Top score.py

--- Final server/Top score.py
+++ Final server/Top score.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+
+# Turn on debug mode.
+import cgitb
+import pymysql
+import json
+
+cgitb.enable()
+
+# Print necessary headers.
+print("Content-Type: text/html; charset=utf-8\n\n")
+
+import cgi
+form = cgi.FieldStorage()
+
+# Connect to the database.
+conn = pymysql.connect(
+    db='highscores',
+    user='root',
+    passwd='samwillsfal',
+    host='localhost')
+c = conn.cursor()
+
 def print_highscore():
     c.execute("SELECT players.first_name, players.last_name, scores.score FROM scores INNER JOIN players ON player_id = players.id ORDER BY score DESC LIMIT 1")
     print ("<b>Top score</b><br />")
